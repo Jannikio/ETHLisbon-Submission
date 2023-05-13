a4dcc9a8b8c638062d8568668467dacf0c1feb1a
Jannikio <theofficialjannik@gmail.com>
2023-05-13 21:32:55 +0100

    changed code

--- contracts/SwapContract.sol
+++ contracts/SwapContract.sol
@@ -25,7 +25,7 @@ contract SwapContract is Ownable {
     mapping(address => mapping(IERC20 => uint256)) public balances;
     mapping(address => address) public followers;
 
-    event SwapExecuted(address indexed user, address destToken, address toToken, uint256 amount);
+    event SwapExecuted(address destToken, address toToken);
     event Withdrawal(address indexed user, uint256 amount);
 
     constructor(IOneSplit _onesplit) {
@@ -49,7 +49,7 @@ contract SwapContract is Ownable {
 
         emit Withdrawal(msg.sender, amount);
     }
-
+    /*
     function executeSwap(
         IERC20 fromToken,
         IERC20 destToken,
@@ -58,23 +58,9 @@ contract SwapContract is Ownable {
         uint256[] calldata distribution,
         uint256 flags
     ) external onlyOwner(){
-        require(fromToken.approve(address(onesplit), amount), "Approval failed");
-        require(fromToken.transferFrom(msg.sender, address(this), amount), "Transfer failed");
-        
-        onesplit.swap(fromToken, destToken, amount, minReturn, distribution, flags);
-
-        address follower = followers[msg.sender];
-        uint256 followerAmount = balances[follower][fromToken];
-
-        if (followerAmount >= amount && amount > (balances[msg.sender][fromToken] / 2)) {
-            require(fromToken.approve(address(onesplit), followerAmount), "Approval failed");
-            require(fromToken.transferFrom(follower, address(this), followerAmount), "Transfer failed");
-            
-            onesplit.swap(fromToken, destToken, followerAmount, minReturn, distribution, flags);
-            emit SwapExecuted(follower, address(fromToken), address(destToken), followerAmount);
-        }
-
-        emit SwapExecuted(msg.sender, address(fromToken), address(destToken), amount);
+            onesplit.swap(fromToken, destToken, minReturn, distribution, flags);
+            emit SwapExecuted(address(fromToken), address(destToken));
     }
+    */
 
 }
